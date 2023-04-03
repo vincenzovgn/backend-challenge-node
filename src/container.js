055b2f29324b036fb14865be6d9fab7fc7487af2
vincenzo <vincenzo@DESKTOP-HS1D480.localdomain>
2023-04-03 16:52:27 -0300

    feat: add dependencies

--- src/container.js
+++ src/container.js
@@ -4,7 +4,9 @@ const container = awilix.createContainer();
 const Config = require('../config');
 const Server = require('./interfaces/http/Server');
 const Application = require('./app/application');
-const Router = require('./interfaces/http/router');
+const Router = require('./interfaces/http/Router');
+const sequelize = require('./infra/database/models');
+// const Exception = require('./infra/errors/Exceptions');
 
 container
   .register({
@@ -12,14 +14,26 @@ container
     container: awilix.asValue(container),
     server: awilix.asClass(Server).singleton(),
     application: awilix.asClass(Application).singleton(),
-    router: awilix.asFunction(Router).singleton()
+    router: awilix.asFunction(Router).singleton(),
+    sequelize: awilix.asFunction(sequelize).singleton()
+    // exception: awilix.asFunction(Exception)
   })
-  .loadModules(['src/interfaces/http/middlewares/**/*.js'], {
-    formatName: 'camelCase',
-    resolverOptions: {
-      injectionMode: awilix.InjectionMode.PROXY,
-      lifetime: awilix.Lifetime.SINGLETON
+  .loadModules(
+    [
+      'src/interfaces/http/middlewares/**/*.js',
+      'src/interfaces/http/controllers/**/*.js',
+      'src/app/operation/**/*.js',
+      'src/app/services/**/*.js',
+      'src/infra/repositories/**/*.js',
+      'src/infra/errors/**/*.js'
+    ],
+    {
+      formatName: 'camelCase',
+      resolverOptions: {
+        injectionMode: awilix.InjectionMode.PROXY,
+        lifetime: awilix.Lifetime.SINGLETON
+      }
     }
-  });
+  );
 
 module.exports = container;
